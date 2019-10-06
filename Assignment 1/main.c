aa88bf59f47e7e393c1d6db9d2476d024f8e5050
ankent <43692048+ankent@users.noreply.github.com>
2019-10-06 12:01:38 -0700

    Add files via upload

---
+++ Assignment 1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int eger = 42;
+    char letter = 'I';
+    long int man = 100l;
+    float boat = 1.2;
+
+    printf("Ann Kent Assignment 1 \n");
+    printf("Hello world!\n");
+    printf("Int: %d\n",eger);
+    printf("Char: %c\n",letter);
+    printf("Long: %ld\n",man);
+    printf("Float: %f\n",boat);
+    return 0;
+}
+
